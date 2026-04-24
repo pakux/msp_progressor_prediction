de51a0a5da86e9057784de886e2a069ef7dd61d4
pakux <paul@42degreesoffreedom.com>
2026-04-24 17:31:08 +0200

    add tables.py

---
+++ notebooks/tables.py
@@ -0,0 +1,45 @@
+import marimo
+
+__generated_with = "0.19.8"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas as pd
+
+
+    baseline_characteristics_df = pd.read_csv('baseline_characteristics.csv')
+    len(baseline_characteristics_df.query('sex == "female"'))
+
+    return baseline_characteristics_df, pd
+
+
+@app.cell
+def _(baseline_characteristics_df, pd):
+    demographics = {}
+
+    demographics['sex'] = [len(baseline_characteristics_df.query('sex in ["male", "female"]')),    len(baseline_characteristics_df.query('sex == "female"'))]
+
+    demographics['age'] = [len(baseline_characteristics_df.query('age.notnull()')), baseline_characteristics_df.age.mean(), baseline_characteristics_df.age.std() ]
+    demographics['mstype'] = [len(baseline_characteristics_df.query('mstype.notnull()'))]
+
+
+    demographics['pdds'] = [len(baseline_characteristics_df.query('pdds_scr.notnull()')), 
+                        
+                            baseline_characteristics_df.pdds_scr.mean(), baseline_characteristics_df.pdds_scr.std()]
+
+
+    demographics['mstype_cis'] = [pd.NA ,len(baseline_characteristics_df.query('mstype == "Clinically Isolated Syndrome"'))]
+    demographics['mstype_rrms'] = [pd.NA ,len(baseline_characteristics_df.query('mstype == "Relapsing Remitting MS"'))]
+    demographics['mstype_spms'] = [pd.NA ,len(baseline_characteristics_df.query('mstype == "Secondary Progressive MS"'))]
+    demographics['mstype_ppms'] = [pd.NA ,len(baseline_characteristics_df.query('mstype == "Primary Progressive MS"'))]
+
+
+    demographics
+    return
+
+
+if __name__ == "__main__":
+    app.run()
